8fae17d12b23e1461087c97bf72381918ca7debd
Bruno Petrolini <brunopetrolini@MacBook-Pro-de-Bruno.local>
2019-11-16 11:31:38 -0300

    add images and product header

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/data/product_data.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 class DetailScreen extends StatefulWidget {
   final ProductData product;
@@ -15,10 +16,53 @@ class _DetailScreenState extends State<DetailScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final Color primaryColor = Theme.of(context).primaryColor;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(product.title),
         centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.9,
+            child: Carousel(
+              images: product.images.map((url) {
+                return NetworkImage(url);
+              }).toList(),
+              dotSize: 6,
+              dotSpacing: 20,
+              dotBgColor: Colors.transparent,
+              dotColor: primaryColor,
+              dotIncreasedColor: primaryColor,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
